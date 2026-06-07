570c5ec698e11c5b9ff320c6ae8f215746145c96
OAkacio <victor.mor.acacio@gmail.com>
2026-06-07 04:02:34 -0300

    Atualiza constantes de zona habitável e ajusta coeficientes para refletir novos dados

--- src/astraeos_core/utils.py
+++ src/astraeos_core/utils.py
@@ -22,23 +22,50 @@ au_cgs = const.au.cgs.value  # Distância Astronômica em centímetros [cm]
 # * ============================================
 # * Constantes de Zona Habitável
 # * ============================================
-# ? --- Borda Interna (Limite de Estufa Úmida) ---
+# ? --- Borda Interna (Recent Venus) ---
 Seff_sun_int = (
-    1.0146  # Fluxo solar efetivo para a borda interna da zona habitável [Lsol/AU²]
+    1.7763  # Fluxo solar efetivo [Lsol/AU²]
 )
-a_int = 8.1884e-5  # Coeficiente a para a borda interna da zona habitável [adm]
-b_int = 1.9394e-9  # Coeficiente b para a borda interna da zona habitável [adm]
-c_int = -4.3618e-12  # Coeficiente c para a borda interna da zona habitável [adm]
-d_int = -6.8260e-16  # Coeficiente d para a borda interna da zona habitável [adm]
+a_int = 1.4335e-4  # Coeficiente a [adm]
+b_int = 3.3954e-9  # Coeficiente b [adm]
+c_int = -7.6364e-12  # Coeficiente c [adm]
+d_int = -1.1950e-15  # Coeficiente d [adm]
 
-# ? --- Borda Externa (Limite de Estufa Máxima) ---
-Seff_sun_ext = (
-    0.3507  # Fluxo solar efetivo para a borda externa da zona habitável [Lsol/AU²]
+# ? --- Borda Interna (Runaway Greenhouse) ---
+Seff_sun_int = (
+    1.7763  # Fluxo solar efetivo [Lsol/AU²]
 )
-a_ext = 5.9578e-5  # Coeficiente a para a borda externa da zona habitável [adm]
-b_ext = 1.6707e-9  # Coeficiente b para a borda externa da zona habitável [adm]
-c_ext = -3.0058e-12  # Coeficiente c para a borda externa da zona habitável [adm]
-d_ext = -5.1925e-16  # Coeficiente d para a borda externa da zona habitável [adm]
+a_int = 1.2456e-4  # Coeficiente a [adm]
+b_int = 1.4612e-8  # Coeficiente b [adm]
+c_int = -7.6345e-12  # Coeficiente c [adm]
+d_int = -1.7511e-15  # Coeficiente d [adm]
+
+# ? --- Borda Interna (Moist Greenhouse) ---
+Seff_sun_int = (
+    1.0146  # Fluxo solar efetivo [Lsol/AU²]
+)
+a_int = 8.1884e-5  # Coeficiente a [adm]
+b_int = 1.9394e-9  # Coeficiente b [adm]
+c_int = -4.3618e-12  # Coeficiente c [adm]
+d_int = -6.8260e-16  # Coeficiente d [adm]
+
+# ? --- Borda Externa (Maximum Greenhouse) ---
+Seff_sun_ext = (
+    0.3507  # Fluxo solar efetivo [Lsol/AU²]
+)
+a_ext = 5.9578e-5  # Coeficiente a [adm]
+b_ext = 1.6707e-9  # Coeficiente b [adm]
+c_ext = -3.0058e-12  # Coeficiente c [adm]
+d_ext = -5.1925e-16  # Coeficiente d [adm]
+
+# ? --- Borda Externa (Early Mars) ---
+Seff_sun_ext = (
+    0.3207  # Fluxo solar efetivo [Lsol/AU²]
+)
+a_ext = 5.4471e-5  # Coeficiente a [adm]
+b_ext = 1.5275e-9  # Coeficiente b [adm]
+c_ext = -2.1709e-12  # Coeficiente c [adm]
+d_ext = -3.8282e-16  # Coeficiente d [adm]
 
 # ? --- Coeficientes de Zona Habitável Clássica ---
 Teq_int = 303  # Temperatura de equilíbrio para a borda interna da zona habitável [K]
