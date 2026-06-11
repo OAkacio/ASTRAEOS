e57bdd2383fda3db7f69caa5ef1b283f19c2db08
OAkacio <victor.mor.acacio@gmail.com>
2026-06-11 16:49:47 -0300

    Melhora dormatação de utils

--- src/astraeos_core/utils.py
+++ src/astraeos_core/utils.py
@@ -1,11 +1,19 @@
+#
+# * ========================================================================================
+# * Importações
+# * ========================================================================================
+# ? Carregamento dos módulos e dependências locais
+# * ========================================================================================
 try:
     from .lib import *
 except ImportError:
     from lib import *
 
-# * ============================================
+# * ========================================================================================
 # * Constantes Físicas
-# * ============================================
+# * ========================================================================================
+# ? Definição de constantes físicas e astronômicas fundamentais
+# * ========================================================================================
 mp = const.m_p.cgs.value  # Massa do próton [g]
 kb = const.k_B.cgs.value  # Constante de Boltzmann [erg/K]
 G = const.G.cgs.value  # Constante gravitacional [cm³g⁻¹s⁻²]
@@ -19,52 +27,49 @@ pi = np.pi  # Valor da constante matemática PI
 au_cgs = const.au.cgs.value  # Distância Astronômica em centímetros [cm]
 
 
-# * ============================================
+# * ========================================================================================
 # * Constantes de Zona Habitável (Interna)
-# * ============================================
+# * ========================================================================================
+# ? Coeficientes e limites de fluxo para bordas internas teóricas e empíricas
+# * ========================================================================================
+
 # ? --- Borda Interna (Recent Venus) --- [empirico/otimista: Fluxo de energia recebida por Vênus no momento que 'morreu']
-Seff_sun_int_rv = (
-    1.7763  # Fluxo solar efetivo [Lsol/AU²]
-)
+Seff_sun_int_rv = 1.7763  # Fluxo solar efetivo [Lsol/AU²]
 a_int_rv = 1.4335e-4  # Coeficiente a [adm]
 b_int_rv = 3.3954e-9  # Coeficiente b [adm]
 c_int_rv = -7.6364e-12  # Coeficiente c [adm]
 d_int_rv = -1.1950e-15  # Coeficiente d [adm]
 
 # ? --- Borda Interna (Runaway Greenhouse) --- [teórico: Efeito estufa descontrolado]
-Seff_sun_int_rg = (
-    1.0385  # Fluxo solar efetivo [Lsol/AU²]
-)
+Seff_sun_int_rg = 1.0385  # Fluxo solar efetivo [Lsol/AU²]
 a_int_rg = 1.2456e-4  # Coeficiente a [adm]
 b_int_rg = 1.4612e-8  # Coeficiente b [adm]
 c_int_rg = -7.6345e-12  # Coeficiente c [adm]
 d_int_rg = -1.7511e-15  # Coeficiente d [adm]
 
 # ? --- Borda Interna (Moist Greenhouse) --- [teórico/conservador: Atmosfera saturada de vapos d'água]
-Seff_sun_int_mg = (
-    1.0146  # Fluxo solar efetivo [Lsol/AU²]
-)
+Seff_sun_int_mg = 1.0146  # Fluxo solar efetivo [Lsol/AU²]
 a_int_mg = 8.1884e-5  # Coeficiente a [adm]
 b_int_mg = 1.9394e-9  # Coeficiente b [adm]
 c_int_mg = -4.3618e-12  # Coeficiente c [adm]
 d_int_mg = -6.8260e-16  # Coeficiente d [adm]
 
-# * ============================================
+
+# * ========================================================================================
 # * Constantes de Zona Habitável (Externa)
-# * ============================================
+# * ========================================================================================
+# ? Coeficientes e limites de fluxo para bordas externas teóricas e empíricas
+# * ========================================================================================
+
 # ? --- Borda Externa (Maximum Greenhouse) --- [teórico/conservador: saturação atmosférica de CO2 (biológico) para o aquecimento causando intenso efeito Rayleigh e esfriando mais o planeta ao invés de aquecer]
-Seff_sun_ext_mg = (
-    0.3507  # Fluxo solar efetivo [Lsol/AU²]
-)
+Seff_sun_ext_mg = 0.3507  # Fluxo solar efetivo [Lsol/AU²]
 a_ext_mg = 5.9578e-5  # Coeficiente a [adm]
 b_ext_mg = 1.6707e-9  # Coeficiente b [adm]
 c_ext_mg = -3.0058e-12  # Coeficiente c [adm]
 d_ext_mg = -5.1925e-16  # Coeficiente d [adm]
 
 # ? --- Borda Externa (Early Mars) --- [empirico/otimista: Fluxo de energia recebida por Marte no momento que 'morreu']
-Seff_sun_ext_em = (
-    0.3207  # Fluxo solar efetivo [Lsol/AU²]
-)
+Seff_sun_ext_em = 0.3207  # Fluxo solar efetivo [Lsol/AU²]
 a_ext_em = 5.4471e-5  # Coeficiente a [adm]
 b_ext_em = 1.5275e-9  # Coeficiente b [adm]
 c_ext_em = -2.1709e-12  # Coeficiente c [adm]
@@ -74,8 +79,11 @@ d_ext_em = -3.8282e-16  # Coeficiente d [adm]
 Teq_int = 303  # Temperatura de equilíbrio para a borda interna da zona habitável [K]
 Teq_ext = 185  # Temperatura de equilíbrio para a borda externa da zona habitável [K]
 
-# * ============================================
+
+# * ========================================================================================
 # * Constantes de Blindagem Magnetosférica
-# * ============================================
+# * ========================================================================================
+# ? Parâmetros de raio de segurança da magnetosfera planetária
+# * ========================================================================================
 Rseg_terra_at = 10.2  # Raio Padrão de Segurança da Magnetosfera (magnetosfera atual do planeta Terra) [Raios Planetários]
 Rseg_terra_min = 5  # Raio Mínimo de Segurança Magnetosfera (magnetosfera paleoarqueana do planeta Terra) [Raios Planetários]
